d21669c5493c10aff06e2e050e90cb5656eb029a
aless <la248911@student.helha.be>
2026-05-20 16:50:12 +0200

    Ajout d'un menu simple avec selection des pions et pseudo à rentrer

--- view/menu.php
+++ view/menu.php
@@ -4,11 +4,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TicTacToe - Menu Principal</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <title>Menu Principal - TicTacToe</title>
 </head>
 
 <body>
-    <a href="partie.php">Jouer</a>
+    <h1>TicTacToe</h1>
+    <form action="partie.php" method="post">
+        <label class="pseudo">Votre pseudo</label> <br>
+        <input type="text" name="pseudo" placeholder="Entrez votre pseudo">
+
+        <p>Choisissez votre pion</p>
+        <div class="pions">
+            <label class="pion-label">
+                <input type="radio" name="pion" value="1" checked>
+                <img src="../assets/croix.png" alt="Croix">
+            </label>
+            <label class="pion-label">
+                <input type="radio" name="pion" value="0">
+                <img src="../assets/cercle.png" alt="Cercle">
+            </label>
+        </div>
+
+        <br>
+        <button type="submit" class="btn-jouer">JOUER</button>
+    </form>
 </body>
 
 </html>
