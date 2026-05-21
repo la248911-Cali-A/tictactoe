a690855cda18142b4904f4aa33c0a2817844f991
aless <la248911@student.helha.be>
2026-05-21 10:34:56 +0200

    - Ajout d'un index.php redirigeant vers menu.php - Correction du CSS : ajout de classes pour le formulaire du menu - Ajout de la grille de jeu dans partie.php

--- view/menu.php
+++ view/menu.php
@@ -10,18 +10,18 @@
 
 <body>
     <h1>TicTacToe</h1>
-    <form action="partie.php" method="post">
-        <label class="pseudo">Votre pseudo</label> <br>
-        <input type="text" name="pseudo" placeholder="Entrez votre pseudo">
+    <form class="menuPrincipal" action="partie.php" method="post">
+        <label class="menuPrincipal-pseudo">Votre pseudo</label> <br>
+        <input class="menuPrincpal-texte" type="text" name="pseudo" placeholder="Entrez votre pseudo">
 
         <p>Choisissez votre pion</p>
         <div class="pions">
             <label class="pion-label">
-                <input type="radio" name="pion" value="1" checked>
+                <input type="radio" name="pion" value="croix" checked>
                 <img src="../assets/croix.png" alt="Croix">
             </label>
             <label class="pion-label">
-                <input type="radio" name="pion" value="0">
+                <input type="radio" name="pion" value="cercle">
                 <img src="../assets/cercle.png" alt="Cercle">
             </label>
         </div>
--- view/partie.php
+++ view/partie.php
@@ -9,6 +9,34 @@
 </head>
 
 <body>
+    <?php
+    if (empty($_POST["pion"])) {
+        header("Location: menu.php");
+        exit();
+    } else {
+        $pionJoueur = $_POST["pion"];
+    }
+    $pionOrdi = ($pionJoueur == "croix") ? "cercle" : "croix";
+    $grille = array_fill(0, 4, array_fill(0, 4, null));
+
+    echo "<table class='grille'>";
+    foreach ($grille as $ligne => $colonnes) {
+        echo '<tr>';
+        foreach ($colonnes as $col => $entree) {
+            if ($entree === "croix") {
+                $affichage = '<img src="../assets/croix.png" alt="X">';
+            } elseif ($entree === "cercle") {
+                $affichage = '<img src="../assets/cercle.png" alt="O">';
+            } else {
+                $affichage = '';
+            }
+            echo "<td data-row=\"{$ligne}\" data-col=\"{$col}\">$affichage</td>";
+        }
+        echo '</tr>';
+    }
+    echo '</table>';
+
+    ?>
 
 </body>
 
